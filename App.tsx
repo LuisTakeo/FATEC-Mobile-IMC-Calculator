d86d82b2087a36abcf1c270e92375f4aeeef9e24
Luis Takeo <110575504+LuisTakeo@users.noreply.github.com>
2026-03-11 19:46:11 +0000

    chore: add package.json and tsconfig.json for project setup
    - Created package.json with dependencies for React Native and navigation libraries.
    - Added tsconfig.json extending Expo's base TypeScript configuration.

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App()
+{
+    return (
+        <view>
+            <Text>
+                Hello World
+            </Text>
+        </view>
+    )
+}
